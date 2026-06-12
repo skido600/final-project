8bcacb0be1484cf67de0d1269a388bb161779bbb
Chuks <63956312+skido600@users.noreply.github.com>
2026-06-13 01:58:41 +0200

    alldone

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,7 +5,7 @@ import authroute from "./routes/user_route.ts";
 import cors from "cors";
 import doctorroute from "./routes/doctor_route.ts";
 import symptomroute from "./routes/symptom.ts";
-
+import type { Request, Response } from "express";
 config();
 const server = express();
 server.use(
@@ -18,6 +18,10 @@ server.use(express.json());
 server.use("/api/auth", authroute);
 server.use("/api/doctor", doctorroute);
 server.use("/api/ai", symptomroute);
+
+server.get("/health", (req: Request, res: Response) => {
+  return res.json({ message: "Server is healthy!" });
+});
 server.use(HandleError);
 
 server.use(notFound);
