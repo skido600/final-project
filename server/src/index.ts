7a7b3fdabded20404f133e0fadaa13fc9ae7565f
Chuks <63956312+skido600@users.noreply.github.com>
2026-06-13 02:27:41 +0200

    alldone

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,10 @@ config();
 const server = express();
 server.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://final-project-ewsz.onrender.com",
+    ],
     credentials: true,
   }),
 );
